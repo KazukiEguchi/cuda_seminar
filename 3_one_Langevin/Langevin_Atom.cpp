6edc6270cfd0fec2e43aa0f9d054891a0887cee5
KazukiEguchi <mailto:eguchi.kazuki@e.mbox.nagoya-u.ac.jp>
2021-04-02 15:46:14 +0900

    add strict code

---
+++ 3_one_Langevin/Langevin_Atom.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <random>
+#include <fstream>
+#include <curand.h>
+#include <curand_kernel.h>
+#include "particle.cuh"
+
+using namespace std;
+
+//粒子数
+#define N 1
+
+void copyH2D(void* dest,void* src,std::size_t size){
+  cudaMemcpy(dest,src,size,cudaMemcpyHostToDevice);
+}
+
+void copyD2H(void* dest,void* src,std::size_t size){
+  cudaMemcpy(dest,src,size,cudaMemcpyDeviceToHost);
+}
+
+//curandStateの初期化
+__global__ void setCurand(unsigned long long seed, curandState *state){
+    uint i_global = threadIdx.x + blockIdx.x*blockDim.x;
+    curand_init(seed, i_global, 0, &state[i_global]);
+}
+
+//Ligevin eq
+__global__ void eq_motion(Atoms atom,double dt,double mass){
+  uint idx = threadIdx.x + blockIdx.x*blockDim.x;
+  atom.d_vx[idx] += -atom.d_vx[idx] * dt /mass + sqrt(2. *dt)* curand_normal_double(&atom.random_fx[idx])/mass;
+  atom.d_vy[idx] += -atom.d_vy[idx] * dt /mass + sqrt(2. *dt)* curand_normal_double(&atom.random_fy[idx])/mass;
+  atom.d_x[idx] += atom.d_vx[idx]*dt;
+  atom.d_y[idx] += atom.d_vy[idx]*dt;
+}
+
+void E_15_ofstream(ofstream *file){
+  char filename[256];
+  sprintf(filename,"time_config_speed.dat");
+
+  (*file).open(filename);
+  vector<ofstream*> of_file{file};
+
+  for(int i = 0;i < 1;i++){
+    (*of_file[i]).setf(ios::scientific);
+    (*of_file[i]).precision(15);
+  }
+}
+
+void output(Atoms atom,ofstream *file){
+  copyD2H(x,d_x,size);
+  copyD2H(y,d_y,size);
+  copyD2H(vx,d_vx,size);
+  copyD2H(vy,d_vy,size);
+  for(int k = 0;k < N;k++){
+    *file << t << " "<< atom.x[k] << " " << atom.y[k] << " " << atom.vx[k] << " " << atom.vy[k] << endl;
+  }
+}
+
+int main(){
+  double t;
+  double dt = 0.01,mass = 1.,time_max = 100.;
+  int threads = 1;
+  int blocks = 1;
+
+  Atoms atom(N);
+  atom.Velocity_conf_zero();
+
+  ofstream file;
+  E_15_ofstream(&file);
+  for(t = 0;t <= time_max;t += dt){
+    eq_motion<<<blocks,threads>>>(atom,dt,mass);
+    output(atom,&file);
+  }
+
+  file.close();
+}
---
+++ 3_one_Langevin/particle.cuh
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <random>
+#include <curand.h>
+#include <curand_kernel.h>
+
+using namespace std;
+
+//curandStateの初期化
+__global__ void setCurand(unsigned long long seed, curandState *state){
+    uint i_global = threadIdx.x + blockIdx.x*blockDim.x;
+    curand_init(seed, i_global, 0, &state[i_global]);
+}
+
+struct Atoms{
+  int N;
+  double *x,*y,*vx,*vy;
+  double *d_x,*d_y,*d_vx,*d_vy;
+  //乱数の状態
+  curandState *random_fx,*random_fy;
+  int threads;
+  int blocks;
+
+  Atoms(int _N){
+    threads = 1;
+    blocks = 1;
+    //シード値
+    random_device seed_gen;
+    int size = N * sizeof(double);
+    N = _N;
+    //allocate host memory
+    x = (double*)malloc(size);
+    y = (double*)malloc(size);
+    vx = (double*)malloc(size);
+    vy= (double*)malloc(size);
+
+    //allocate device memory
+    cudaMalloc(&d_x,size);
+    cudaMalloc(&d_y,size);
+    cudaMalloc(&d_vx,size);
+    cudaMalloc(&d_vy,size);
+    cudaMalloc(&random_fx,N * sizeof(curandState));
+    cudaMalloc(&random_fy,N * sizeof(curandState));
+
+    //シードを使ってcurandStateを初期化
+    setCurand<<<blocks,threads>>>(seed_gen(), random_fx);
+    //シードを使ってcurandStateを初期化
+    setCurand<<<blocks,threads>>>(seed_gen(), random_fy);
+  }
+
+  ~Atoms(){
+    //free host memory
+    free(x);free(y);free(vx);free(vy);
+    //free device memory
+    cudaFree(d_x);cudaFree(d_y);cudaFree(d_vx);cudaFree(d_vy);cudaFree(random_fx);cudaFree(random_fy);
+  }
+
+  __global__ void Velocity_conf_zero(){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if(idx < N){
+      d_vx[idx] = 0.0;
+      d_vy[idx] = 0.0;
+      d_x[idx] = 0.0;
+      d_y[idx] = 0.0;
+    }
+  }
+
+  __global__ void Initial_conf(double L){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    random_device seed;
+    curandState *random_x,*random_y;
+    cudaMalloc(&random_fx,N * sizeof(curandState));
+    cudaMalloc(&random_fy,N * sizeof(curandState));
+    setCurand<<<blocks,threads>>>(seed(), random_x);
+    setCurand<<<blocks,threads>>>(seed(), random_y);
+
+    if(idx < N){
+      d_x[idx] = L * curand_uniform_double(&random_x[idx]);
+      d_y[idx] = L * curand_uniform_double(&random_y[idx]);
+    }
+
+    cudaFree(random_x);cudaFree(random_y);
+  }
+}
